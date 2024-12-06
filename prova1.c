68d715a8f104ba0d82e86bb27387913c48adb38f
Élvis Corrêa Miranda Júnior <138946211+neatzzy@users.noreply.github.com>
2024-12-05 22:28:29 -0300

    Correção do Exercício
    Fala Gabriel, tudo bem?
    
    Fiz uma correção do seu código. Muito provavelmente o erro da sua resolução foi no uso dos vetores, que não eram necessários para resolver a questão, já que nela só precisamos acessar o valor que acabamos de ler. Além disso, eu entendi a condicional caso apenas tenhamos que ler 1 valor, mas não acho que seja uma boa prática resolver dessa maneira, já que isso também pode promover erros no seu código. Fora isso, estava tudo bem organizado, espero que você consiga ir melhor nas próximas provas. Continue assim!

--- prova1.c
+++ prova1.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 
+double Distancia(double x1, double y1, double x2, double y2) {
+    return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+}
+
 int main()
 {
-    int c, n, i, j, numeroDaBola;
-    double xb, yb, distancia;
+    int c, n, numeroDaBola;
+    double xb, yb, dist, menorDistancia;
     
     scanf("%d", &c);
     
-    for (i=0;i<c;i++) {
+    for (int i=0 ; i<c ; i++) {
         scanf("%d", &n);
         
-        double xn[n], yn[n];
+        double xn, yn;
         
         scanf("%lf %lf", &xb, &yb);
         
-        scanf("%lf %lf", &xn[0], &yn[0]);
-        
-        distancia = sqrt((xb-xn[0])*(xb-xn[0]) + (yb-yn[0])*(yb-yn[0]));
-        
-        if (n == 1) {
-            numeroDaBola = 1;
-        } else {
-            for(j=1;j<n;j++) {
-                scanf("%lf %lf", &xn[j], &yn[j]);
-                if (distancia > sqrt(((xb-xn[j])*(xb-xn[j])) + ((yb-yn[j])*(yb-yn[j])))){
-                    distancia = sqrt(((xb-xn[j])*(xb-xn[j])) + ((yb-yn[j])*(yb-yn[j])));
+        for(int j=0 ; j < n ; j++){
+            scanf("%lf %lf", &xn, &yn);
+
+            if(!j){
+                menorDistancia = Distancia(xb, yb, xn, yn);
+                numeroDaBola = j+1;
+            } else {
+                dist = Distancia(xb, yb, xn, yn);
+                if(dist < menorDistancia){
+                    menorDistancia = dist;
                     numeroDaBola = j+1;
-                    }
+                }
             }
         }
         printf("%d\n", numeroDaBola);
-        
     }
-    
     return 0;
 }
